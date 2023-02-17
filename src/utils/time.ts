d1822f5351bd25cbe43d88b97dc6fd1fad0c806c
Xavi Amorós <xaviamoros@gmail.com>
2023-02-17 16:11:46 +0100

    Add time utils

---
+++ src/utils/time.ts
@@ -0,0 +1,30 @@
+type ConvertedTimeType = {
+  hours: string,
+  minutes: string,
+  seconds: string
+}
+
+export const convertSecondsToTime = (inputSeconds: number): ConvertedTimeType  => {
+  const hours = Math.floor(inputSeconds / 3600);
+  const minutes = Math.floor((inputSeconds % 3600) / 60);
+  const seconds = Math.floor(inputSeconds % 60);
+
+  return {
+    hours: hours.toLocaleString(undefined, {minimumIntegerDigits: 2}),
+    minutes: minutes.toLocaleString(undefined, {minimumIntegerDigits: 2}),
+    seconds: seconds.toLocaleString(undefined, {minimumIntegerDigits: 2})
+  };
+}
+
+export const convertSecondsToFullTime = (inputSeconds: number): string  => {
+  const { hours, minutes, seconds } = convertSecondsToTime(inputSeconds);
+  return `${hours}:${minutes}:${seconds}`
+}
+
+
+
+export const timestampToTime = (timestamp) => {
+  return {
+
+  }
+}
